cb442242bd1f4fc8b7bda2bb2488916480f33dee
akalManNakarmi <akalnakarmi1@gmail.com>
2023-02-04 15:36:14 +0545

    lab8
    matrix multiplication

---
+++ lab8/q2.c
@@ -0,0 +1,14 @@
+// WAP to bubble sort array the user enters
+
+#include <stdio.h>
+int main(){
+    int a_len;
+    printf("Enter length: ");
+    scanf("%d",&a_len);
+    printf("Enter array");
+    int a[a_len]
+    for(int i=0;i<a_len;i++){
+        
+    }
+    return 0;
+}
